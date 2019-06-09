139397bdef3ca8f2e41921d677716953a3c303fb
余文彪 <yuwb@corp.21cn.com>
2019-06-09 18:53:53 +0800

    Initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+/**
+ * 以上两个注解可以通过启动项目来进行单元测试
+ */
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
