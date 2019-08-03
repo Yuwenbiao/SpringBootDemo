7f42e7b3d67f1428129f88fd42056e40b5c126d2
余文彪 <yuwb@corp.21cn.com>
2019-08-03 17:13:19 +0800

    实现Web层的日志切面

---
+++ src/main/java/com/example/demo/aspect/WebLogAspect.java
@@ -0,0 +1,55 @@
+package com.example.demo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * 实现Web层的日志切面
+ *
+ * @author yuwb@corp.21cn.com
+ * @date 2019/8/3 16:46
+ */
+@Aspect
+@Component
+public class WebLogAspect {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * com.example.demo.controller包下的所有函数（对controller层所有请求处理做切入点）
+     */
+    @Pointcut("execution(public * com.example.demo.controller..*.*(..))")
+    public void webLog() {
+    }
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) {
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        logger.info("URL : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) {
+        // 处理完请求，返回内容
+        logger.info("RESPONSE : " + ret);
+    }
+}
---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * AOP测试类
+ *
+ * @author yuwb@corp.21cn.com
+ * @date 2019/8/3 16:41
+ */
+@RestController
+public class HelloController {
+    /**
+     * 打印Hello
+     *
+     * @param name 姓名
+     * @return 返回Hello 姓名
+     */
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(@RequestParam String name) {
+        return "Hello " + name;
+    }
+}
